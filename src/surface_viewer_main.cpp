214c0165d975a41968fdb53edeb3a524f238583d
abunge <abunge@techfak.uni-bielefeld.de>
2023-04-04 10:26:48 +0200

    Apple CmakeList addition and gitignore

--- src/surface_viewer_main.cpp
+++ src/surface_viewer_main.cpp
@@ -4,11 +4,12 @@
 
 //=============================================================================
 int main(int argc, char **argv) {
-    if(argc < 2){
-        std::cout << "Specify input mesh" << std::endl;
-    }
     Viewer window("Polygon Modeling", 800, 600);
-    window.load_mesh(argv[1]);
+    if(argc < 2){
+        window.load_mesh("../data/surface_meshes/grid/concave_1.obj");
+    }else {
+        window.load_mesh(argv[1]);
+    }
     return window.run();
 }
 //=============================================================================
--- src/volume_viewer_main.cpp
+++ src/volume_viewer_main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv) {
 
     VolumeViewer volumeWindow("Polygon Modeling", 800, 600);
     if(argc < 2){
-        volumeWindow.load_mesh("../data/ovm/cube.ovm");
+        volumeWindow.load_mesh("../data/volume_meshes/cubes/cube_hexahedron_1.ovm");
     }else{
         volumeWindow.load_mesh(argv[1]);
     }
